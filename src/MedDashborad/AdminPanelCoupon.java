160c152620008733bc96514d7f1ae818a7e3d041
Ahmad Malloh <ahmadmalloh97@gmail.com>
2021-01-05 22:34:54 +0200

    arrange the code

--- src/MedDashborad/AdminPanelCoupon.java
+++ src/MedDashborad/AdminPanelCoupon.java
@@ -1,14 +1,16 @@
 package MedDashborad;
 
+import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.Iterator;
 import java.util.Set;
-import java.util.concurrent.TimeUnit;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
@@ -17,7 +19,6 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 import com.relevantcodes.extentreports.LogStatus;
-import atu.testrecorder.ATUTestRecorder;
 import atu.testrecorder.exceptions.ATUTestRecorderException;
 
 public class AdminPanelCoupon extends TestBase {
@@ -28,7 +29,6 @@ public class AdminPanelCoupon extends TestBase {
     	Started();
 	}
     
-	
 	@AfterSuite
 	public void FinishedTesting()
 	{
@@ -38,35 +38,29 @@ public class AdminPanelCoupon extends TestBase {
 	@BeforeMethod
 	public void setup(Method method) throws ATUTestRecorderException
 	{
-		test = extent.startTest(method.getName());
-		recorder = new ATUTestRecorder("D:\\Users\\ahmad\\eclipse-workspace\\Qiotic_Projects\\TestReport",method.getName(),false);
-		recorder.start();
-		ChromeOptions option = new ChromeOptions();
-		option.addArguments("--incognito");
-	    System.setProperty("webdriver.chrome.driver","C:\\Users\\ahmad\\Downloads\\chromedriver_win32 (4)\\chromedriver.exe");
-		driver = new ChromeDriver(option);
-		driver.get("https://medical.qiotic.info/en/admin/login");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.findElement(By.id("username")).sendKeys("admin");
-        driver.findElement(By.id("password")).sendKeys("123456");
-        driver.findElement(By.xpath("//button[text()='Sign In']")).click();
+		before(method.getName());
 	}
 	
 	@AfterMethod
-	public void teardown(ITestResult result, Method method) throws ATUTestRecorderException
+	public void teardown(ITestResult result, Method method) throws ATUTestRecorderException, IOException
 	{
 		recorder.stop();
 		if (result.getStatus() == ITestResult.SUCCESS)
 		{
 			test.log(LogStatus.PASS, "<a href='"+result.getName()+".mov" + "'><span class='lable info'>Download Video</span></a>");
+			test.log(LogStatus.PASS, "<a href='"+result.getName()+".png" + "'><span class='lable info'>Download Snapshot</span></a>");
 		}
 		else if(result.getStatus() == ITestResult.FAILURE)
 		{
 			test.log(LogStatus.FAIL,"<a href='"+result.getName()+".mov" + "'><span class='lable info'>Download Video</span></a>");
+			test.log(LogStatus.FAIL,"<a href='"+result.getName()+".png" + "'><span class='lable info'>Download Snapshot</span></a>");
 		}
 		else 
 			test.log(LogStatus.SKIP,"Test Skipped");
+		
+		File srcfile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE );
+		FileUtils.copyFile(srcfile, new File("D:\\Users\\ahmad\\eclipse-workspace\\Qiotic_Projects\\TestReport\\"+method.getName()+".png"));
+
 		
 		driver.quit();
 	}
--- src/MedDashborad/TestBase.java
+++ src/MedDashborad/TestBase.java
@@ -1,9 +1,16 @@
 package MedDashborad;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 import atu.testrecorder.ATUTestRecorder;
+import atu.testrecorder.exceptions.ATUTestRecorderException;
 
 public class TestBase {
 	
@@ -26,6 +33,24 @@ public class TestBase {
 			extent.flush();
 			System.out.println("The Testing is Finished");
 		}
+	 
+	 public void before(String name) throws ATUTestRecorderException
+	 {
+
+			test = extent.startTest(name);
+			recorder = new ATUTestRecorder("D:\\Users\\ahmad\\eclipse-workspace\\Qiotic_Projects\\TestReport",name,false);
+			recorder.start();
+			ChromeOptions option = new ChromeOptions();
+			option.addArguments("--incognito");
+		    System.setProperty("webdriver.chrome.driver","C:\\Users\\ahmad\\Downloads\\chromedriver_win32 (4)\\chromedriver.exe");
+			driver = new ChromeDriver(option);
+			driver.get("https://medical.qiotic.info/en/admin/login");
+	        driver.manage().window().maximize();
+	        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	        driver.findElement(By.id("username")).sendKeys("admin");
+	        driver.findElement(By.id("password")).sendKeys("123456");
+	        driver.findElement(By.xpath("//button[text()='Sign In']")).click();
+	 }
 	 
 	 
 
